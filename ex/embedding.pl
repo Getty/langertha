09121777e9ee002bc8cd4f5022483ce04ddd360b
Torsten Raudssus <torsten@raudss.us>
2024-07-26 06:04:29 +0200

    Turn ex/embedding into a speed test

--- ex/embedding.pl
+++ ex/embedding.pl
@@ -12,24 +12,66 @@ use Data::Dumper;
 use Langertha::Engine::Ollama;
 use Langertha::Engine::OpenAI;
 
-if ($ENV{OLLAMA_URL}) {
-
-  my $ollama = Langertha::Engine::Ollama->new(
-    url => $ENV{OLLAMA_URL},
-  );
-
-  print Dumper($ollama->simple_embedding("the"));
-
+my @names_list = qw(
+  Harley Ramirez
+  David Lin
+  Makenna Dominguez
+  Kaden Chen
+  Valeria McKinney
+  Romeo Perry
+  Clara Brewer
+  Cruz Gillespie
+  Alianna Stevens
+  Zachary Velez
+  Megan Dougherty
+  Brett Villanueva
+  Monroe Carlson
+  Paul Brooks
+  Autumn Berry
+  Adonis Yates
+  Charley Bowman
+  Francisco McConnell
+  Denise Potter
+);
+my @names;
+while (@names_list) {
+  push @names, join(' ', shift @names_list, shift @names_list);
 }
 
-if ($ENV{OPENAI_API_KEY}) {
+{
+  if ($ENV{OLLAMA_URL}) {
+    my $start = time;
 
-  my $openai = Langertha::Engine::OpenAI->new(
-    api_key => $ENV{OPENAI_API_KEY},
-  );
+    my $ollama = Langertha::Engine::Ollama->new(
+      url => $ENV{OLLAMA_URL},
+    );
 
-  print Dumper($openai->simple_embedding("the"));
+    for my $name (@names) {
+      print ".";
+      $ollama->simple_embedding($name);
+    }
 
+    my $end = time;
+    printf("\n\n%u\n\n", $end - $start);
+  }
+}
+
+{
+  if ($ENV{OPENAI_API_KEY}) {
+    my $start = time;
+
+    my $openai = Langertha::Engine::OpenAI->new(
+      api_key => $ENV{OPENAI_API_KEY},
+    );
+
+    for my $name (@names) {
+      print ".";
+      $openai->simple_embedding($name);
+    }
+
+    my $end = time;
+    printf("\n\n%u\n\n", $end - $start);
+  }
 }
 
 exit 0;
